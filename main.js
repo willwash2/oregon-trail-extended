c0a843f9bc9e869aa8688c10dfa0a848fe23bef6
William Washington <will.da.coder@gmail.com>
2020-07-23 12:00:18 -0400

    finished the assesment

--- main.js
+++ main.js
@@ -19,6 +19,48 @@ class Traveler {
 
     }
 }
+class Doctor extends Traveler {
+    constructor(name) {
+        super(name)
+
+    }
+    heal(traveler) {
+        traveler.isHealthy = true
+    }
+}
+
+class Hunter extends Traveler {
+    constructor(name) {
+        super(name)
+        this.food = 2
+
+    }
+    hunt() {
+        this.food += 5
+
+    }
+    eat() {
+        if (this.food > 1) {
+
+            this.food -= 2
+
+        }
+        else {
+            this.isHealthy = (false)
+            this.food = 0
+        }
+    }
+    giveFood(traveler, numberOfFoodUnits) {
+
+        if (this.food >= numberOfFoodUnits) {
+            this.food -= numberOfFoodUnits
+        }
+
+    }
+}
+
+
+
 
 
 
